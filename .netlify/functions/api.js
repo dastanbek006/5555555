5113e2ef7c30ee35067f6462c459a10bef4fbee4
dastanbek006 <dastanbekfarhadov@gmail.com>
2026-09-14 01:01:21 +0500

    Add Netlify serverless function placeholder

---
+++ .netlify/functions/api.js
@@ -0,0 +1,8 @@
+// Placeholder for API functions
+// This can be used for serverless functions if needed
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "API is working" })
+  };
+};
